a80f1074d65ea2d81eaef336fcee7039465eb7e9
Michał Swędrowski <michal.swedrowski@capgemini.com>
2019-10-21 10:30:25 +0200

    History - ActionBarMenu

--- app/src/main/java/com/oracle/oracle_mobile_app/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/com/oracle/oracle_mobile_app/ui/main/history/HistoryFragment.kt
@@ -1,9 +1,7 @@
 package com.oracle.oracle_mobile_app.ui.main.history
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.mikephil.charting.data.Entry
@@ -12,8 +10,12 @@ import com.oracle.oracle_mobile_app.databinding.FragmentHistoryBinding
 import kotlinx.android.synthetic.main.fragment_history.*
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.data.LineData
-
 import com.oracle.oracle_mobile_app.utils.converters.TimestampValueFormatter
+import android.preference.PreferenceManager
+
+
+
+
 
 
 class HistoryFragment : Fragment() {
@@ -22,6 +24,10 @@ class HistoryFragment : Fragment() {
     private lateinit var binding: FragmentHistoryBinding
     var chart_entries = mutableListOf<Entry>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,16 +35,61 @@ class HistoryFragment : Fragment() {
     ): View? {
         historyViewModel = HistoryViewModel()
 
-
         historyViewModel.historyOrderNumList.value?.forEach { order_amnt ->
             chart_entries.add(Entry(order_amnt.dateTimeStamp.toFloat(),order_amnt.value))
         }
-
 
         val root = inflater.inflate(R.layout.fragment_history, container, false)
 
         return root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu,menuInflater: MenuInflater) {
+        val inflater: MenuInflater = menuInflater
+        val savedIndex = PreferenceManager.getDefaultSharedPreferences(activity).getInt("NAME", 1)
+        for (i in 1..3) {
+            val item = menu.findItem(getMenuIdByIndex(i))
+            if (item != null) {
+                item.isChecked = i == savedIndex
+            }
+        }
+
+        inflater.inflate(R.menu.history_action_bar_menu, menu)
+
+        menu.findItem(R.id.full_history).isChecked = true
+    }
+
+    private fun getMenuIdByIndex(index: Int): Int {
+        when (index) {
+            1 -> return R.id.last_month
+            2 -> return R.id.last_year
+            3 -> return R.id.full_history
+        }
+        return 0
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        item.isChecked = !item.isChecked
+
+        return when(item.itemId){
+            R.id.last_month -> {    PreferenceManager.getDefaultSharedPreferences(activity).edit().putInt("NAME", 1).apply()
+                true
+            }
+
+            R.id.last_year -> {     PreferenceManager.getDefaultSharedPreferences(activity).edit().putInt("NAME", 2).apply()
+                true
+            }
+
+            R.id.full_history -> {  PreferenceManager.getDefaultSharedPreferences(activity).edit().putInt("NAME", 3).apply()
+                true
+            }
+
+            else ->
+                super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
